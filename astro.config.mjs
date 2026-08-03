c83984fb401a62aa628400a2aeab3f212955fd73
Victoria Mulcue <victorialiceth82@gamil.com>
2026-08-02 21:54:46 -0500

    Configure GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://esquinas98.github.io',
+  base: '/ombliburguer/',
   vite: {
     plugins: [tailwindcss()]
   }
